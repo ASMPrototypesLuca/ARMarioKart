35cc85d06b4c11e20d79849db2c9a3c8f404c394
Luca Bulles <luca@Lucas-MacBook-Pro.local>
2023-05-11 13:13:41 +0200

    Replaced application to match IOS restrictions

--- ARFeatureOutdoorRaceUITests/ARFeatureOutdoorRaceUITests.swift
+++ ARFeatureOutdoorRaceUITests/ARFeatureOutdoorRaceUITests.swift
@@ -1,13 +1,13 @@
 //
-//  ARFeatureMarioKartUITests.swift
-//  ARFeatureMarioKartUITests
+//  ARFeatureOutdoorRaceUITests.swift
+//  ARFeatureOutdoorRaceUITests
 //
-//  Created by Luca Bulles on 19/04/2023.
+//  Created by Luca Bulles on 11/05/2023.
 //
 
 import XCTest
 
-final class ARFeatureMarioKartUITests: XCTestCase {
+final class ARFeatureOutdoorRaceUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
